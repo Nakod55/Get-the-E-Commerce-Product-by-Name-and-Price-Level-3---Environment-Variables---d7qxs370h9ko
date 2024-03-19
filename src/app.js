224b243e8a7662adbaa7da07c665fa432ec44f3f
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-20 01:27:22 +0530

    submit

--- src/app.js
+++ src/app.js
@@ -10,5 +10,29 @@ app.use(express.json());
 
 // Write GET endpoint for sending product to the client here
 // Endpoint - /api/v1/products/:name/:price
+app.get("/api/v1/products/:name/:price",(req,res)=>{
+    const  { name , price }= req.params;
+    const product= products.find((e)=>e.price==price&&e.name==name);
+    if(product)
+    {
+        res.status(200).send(
+            {    
+                status: "success", 
+            
+                message: "Product fetched successfully",
+            
+                data: { 
+            
+                    product: product
+            
+                }
+            
+        })
+    }
+    else{
+        res.status(404).send({ "status": "failed", "message": "Product not found!" });
+        
+    }
+})
 
 module.exports = app;
